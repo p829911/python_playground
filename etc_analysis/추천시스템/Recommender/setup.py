dad6d0685f81f4f2798b5be2d6549a27eaebd70d
현승우 <p829911@gmail.com>
2019-01-22 18:27:45 +0900

    upload

---
+++ etc_analysis/추천시스템/Recommender/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name            = 'Recommender',
+    version         = '1.0',
+    description     = 'Python Recommender System Package',
+    author          = 'Seungwoo Hyun',
+    author_email    = 'p829911@gmail.com',
+    url             = 'https://github.com/p829911',
+    download_url    = "https://github.com/p829911/python_study/tree/master/etc_analysis/recommender_system/Recommender",
+    install_requires= ["numpy", "pandas", "scipy"],
+    packages        = find_packages(),
+    keywords        = ["recommender"],
+    python_requires = '>=3'
+)

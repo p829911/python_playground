580d7c35f0796b089b37cd7718939997e95e018c
현승우 <p829911@gmail.com>
2019-01-22 18:18:13 +0900

    upload recommender ver 1.0

---
+++ etc_analysis/추천시스템/Recommender/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name            = 'Recommender',
+    version         = '1.0',
+    description     = 'Python Recommender System Package',
+    author          = 'Seungwoo Hyun',
+    author_email    = 'p829911@gmail.com',
+    url             = , 
+    download_url    = ,
+    install_requires= ["numpy", "pandas", "scipy"],
+    packages        = find_packages(),
+    keywords        = ["recommender"],
+    python_requires = '>=3'
+)
